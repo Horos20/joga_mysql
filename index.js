d846aa53dc090ac602886dbf2317fcc4a655efc3
Matis Rammi <matis.rammi@khk.ee>
2022-02-13 11:26:13 +0200

    Displayed author article list

--- index.js
+++ index.js
@@ -47,16 +47,34 @@ app.get('/', (req, res) => {
 })
 
 app.get('/article/:slug', (req, res) => {
-    let query = `SELECT * , article.name as article_name, author.name as author_name  FROM article inner join author on article.author_id=author.id WHERE slug="${req.params.slug}"`;
+    let query = `SELECT * , article.name as article_name, author.name as author_name, author.id as author_id  FROM article inner join author on article.author_id=author.id WHERE slug="${req.params.slug}"`;
     let article
     con.query(query, (err, result) => {
         if (err) throw err;
         article = result
-        console.log(article)
         res.render('article', {
             article: article
         })
     });
+});
+
+app.get('/author/:author_id', (req, res) => {
+    let query = `SELECT *, article.name AS article_name FROM article INNER JOIN author ON article.author_id = author.id WHERE author.id="${req.params.author_id}"`;
+    let articles = []
+    let author = `select name from author where author.id="${req.params.author_id}"`;
+    let authors = []
+    con.query(query, (err,result) => {
+        if (err) throw err;
+        articles = result
+        con.query(author, (err, result) => {
+            if (err) throw err;
+            authors = result;
+            res.render('author', {
+                authors: authors,
+                articles: articles
+            })
+        })
+    })
 });
 
 app.listen(3001, () => {
